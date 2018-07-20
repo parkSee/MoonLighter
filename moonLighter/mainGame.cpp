70b972f13b45689c98c3c6346bffaaed6a89edbf
DESKTOP-J3GOUGQ\park <soso667@naver.com>
2018-07-20 14:16:32 +0900

    주석처리했습니다

--- moonLighter/mainGame.cpp
+++ moonLighter/mainGame.cpp
@@ -38,7 +38,7 @@ void mainGame::update(void)
 	_player->update();
 
 
-
+	//카메라의 움직임 담당입니다. 안건드려도 되요
 	CAMERAMANAGER->update();
 
 }
@@ -53,11 +53,15 @@ void mainGame::render(void)
 //=============================================================
 	RECT rc = CAMERAMANAGER->getRenderRc();
 
-	//IMAGEMANAGER->render("map", getMemDC(), 0, 0,rc.left,rc.top,WINSIZEX,WINSIZEY);
-	IMAGEMANAGER->render("map", getMemDC(), 0 - rc.left, 0 - rc.top);
+	//카메라가 보여주는 부분까지만 부분 렌더 합니다. 
+	//그래야지 프레임이 덜떨어집니다.
+	IMAGEMANAGER->render("map", getMemDC(), 0, 0,rc.left,rc.top,WINSIZEX,WINSIZEY);
+	//IMAGEMANAGER->render("map", getMemDC(), 0 - rc.left, 0 - rc.top);
 
 	//_background->render(getMemDC(), 0, 0, renderRC.left, renderRC.top, WINSIZEX, WINSIZEY);
 
+	//카메라의 렉트 보여주기 위해서 넣어놓은겁니다. 
+	//거슬리면 주석처리 가능=====================================
 	HPEN pen = CreatePen(PS_SOLID, 3, RGB(255, 0, 0));
 	HPEN oldPen = (HPEN)SelectObject(getMemDC(), pen);
 
@@ -67,8 +71,10 @@ void mainGame::render(void)
 
 	SelectObject(getMemDC(), oldPen);
 	DeleteObject(pen);
+	//=========================================================
 
-	CAMERAMANAGER->render(getMemDC());
+	//카메라 렌더 부분입니다 없어도 작동 잘됩니다
+	//CAMERAMANAGER->render(getMemDC());
 
 	_player->render();
 
--- moonLighter/player.cpp
+++ moonLighter/player.cpp
@@ -11,6 +11,7 @@ HRESULT player::init(void)
 
 	rc = RectMakeCenter(pos.x,pos.y, 70, 70);
 
+	//처음 타겟을 지정해줍니다
 	CAMERAMANAGER->connectTarget(pos.x, pos.y);
 	
 	return S_OK;
@@ -23,6 +24,11 @@ void player::release(void)
 
 void player::update(void)
 {
+	//업데이트에서 계속해서 타겟팅될 아이의 좌표를 넣어줍니다.
+	//그래야지 그 좌표를 따라 움직입니다.
+	//에너미로 카메라를 바꾸고 싶으면 플레이어쪽에 connectTarget을 주석처리하고 
+	//에너미 업데이트에 connectTarget를 넣어주면됩니다.
+	//렌더에서 좌표빼는거 잊지마세요!
 	CAMERAMANAGER->connectTarget(pos.x, pos.y);
 
 
@@ -53,6 +59,8 @@ void player::update(void)
 
 void player::render(void)
 {
+
+	//카메라의 rc만큼 플레이어 좌표에서 빼줍니다.
 	RECT cam = CAMERAMANAGER->getRenderRc();
 
 	RectangleMake(getMemDC(), pos.x - cam.left, pos.y - cam.top, 70, 70);
