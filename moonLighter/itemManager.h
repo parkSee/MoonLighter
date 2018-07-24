fdfa75f87b3290863a70f298c44891d4905b2caa
채수윤 <tndbsdlchlrh@naver.com>
2018-07-24 14:41:31 +0900

    아이템매니져 내용 수정

--- moonLighter/itemManager.h
+++ moonLighter/itemManager.h
@@ -1,11 +1,20 @@
 #pragma once
+#include "gameObject.h"
+#include "item.h"
 
-class gameObject;
+// csyADD [아이템 매니저 클래스 추가]
 
-class itemManager :
-	public gameObject
+// 아이템 타입 이넘문
+enum itemType
 {
+	POTION, RIQUIDITEM, REDCRISTAL, TWIG, REBAR, GOLEMCORE, END
+};
 
+
+class itemManager : public gameObject
+{
+	unordered_map<itemType, vector<item*>> _mItem;					// 맵을 이용한 아이템 
+	typedef unordered_map<itemType, vector<item*>>::iterator mItemIter;	// 아이템 반복자
 
 public:
 
