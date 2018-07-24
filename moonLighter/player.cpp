b0d5a4e39f453f1c5ba1ee36243a99c5679ba5a9
이요성 <leeys1551@gmail.com>
2018-07-24 09:41:06 +0900

    플레이어 헤더, cpp 파일 (플레이어 이미지)

--- moonLighter/player.cpp
+++ moonLighter/player.cpp
@@ -1,16 +1,28 @@
 #include "stdafx.h"
 #include "player.h"
-
+#include "mini.h"
 
 
 HRESULT player::init(string _objName, tagFloat _pos)
 {
 	gameObject::init(_objName, _pos);
+	
+	_index = _count = 0;
+	_time = 0.0f;
+	_speed = 3.3f;
+	_acceleration = 1.5f;
+	_isUp = false;
+	_isRight = false;
+	_isRolling = false;
+	_isInvincible = false;
 
-
-
-
-	//============================== 안건드려도됨
+	will = new image;
+	will->init("Image/will_shop2.bmp", 1800, 2160, 10, 12, true, RGB(255, 0, 255));
+	_rc = RectMake(pos.x, pos.y, will->getFrameWidth(), will->getFrameHeight());
+	
+	_probeX = pos.x + (will->getFrameWidth() / 2);
+	_probeY = pos.y + (will->getFrameHeight() / 2);
+	
 	CAMERAMANAGER->connectTarget(pos.x, pos.y);
 	
 	return S_OK;
@@ -25,25 +37,411 @@ void player::update(void)
 {
 	gameObject::update();
 
-	//============================== 안건드려도됨
 	CAMERAMANAGER->connectTarget(pos.x, pos.y);
-	//==========================================
+
+	/*
+	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+	{
+		
+		pos.x -= 5;
+
+		_probeX = pos.x - 35;
+
+		for (int i = _probeX + 5; i >= _probeX - 5; --i)
+		{
+			COLORREF color = GetPixel(IMAGEMANAGER->findImage("radZone")->getMemDC(), i, pos.y);
+			int r = GetRValue(color);
+			int g = GetGValue(color);
+			int b = GetBValue(color);
+
+			if (r == 255 && g == 0 && b == 0)
+			{
+				pos.x = i + 35;
+				break;
+			}
+		}
+
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+	{
+		pos.x += 5;
+
+		_probeX = pos.x + 35;
+
+		for (int i = _probeX - 5; i < _probeX + 5; ++i)
+		{
+			COLORREF color = GetPixel(IMAGEMANAGER->findImage("radZone")->getMemDC(), i, pos.y);
+			int r = GetRValue(color);
+			int g = GetGValue(color);
+			int b = GetBValue(color);
+
+			if (r == 255 && g == 0 && b == 0)
+			{
+				pos.x = i - 35;
+				break;
+			}
+		}
+
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_UP))
+	{
+		pos.y -= 5;
+
+		_probeY = pos.y - 35;
+
+		for (int i = pos.y + 35; i >= pos.y - 35; i--)
+		{
+			COLORREF color = GetPixel(IMAGEMANAGER->findImage("radZone")->getMemDC(), pos.x, i);
+			int r = GetRValue(color);
+			int g = GetGValue(color);
+			int b = GetBValue(color);
+
+			if (r == 255 && g == 0 && b == 0)
+			{
+				pos.y = i + 35;
+				break;
+			}
+		}
+
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+	{
+		pos.y += 5;
+
+		_probeY = pos.y+ 35;
+
+		for (int i = _probeY - 10; i < _probeY + 10; ++i)
+		{
+			COLORREF color = GetPixel(IMAGEMANAGER->findImage("radZone")->getMemDC(), pos.x, i);
+			int r = GetRValue(color);
+			int g = GetGValue(color);
+			int b = GetBValue(color);
+
+			if (r == 255 && g == 0 && b == 0)
+			{
+				pos.y = i - 35;
+				break;
+			}
+		}
 
 
+	}
+	
+	if (KEYMANAGER->isStayKeyDown('T'))
+	{
+		CAMERAMANAGER->shakeCamera(5.0f, 11.0f);
+	}
+	*/
+
+	rc = RectMakeCenter(pos.x, pos.y, 70, 70);
+	
+	
+	//this->collision();
+	
+	move();
 }
 
 void player::render(void)
 {
-	//렉트 확인할때 꼭
+	
 	RECT cam = CAMERAMANAGER->getRenderRc();
 
-	//이런식으로 카메라 렉트 만큼 꼭 빼주세요 번거롭겟지만.. 부탁드립니당
-	//Rectangle(getMemDC(), rc.left - cam.left, rc.top - cam.top,rc.right - cam.left, rc.bottom - cam.top); 
-	
+	//RectangleMake(getMemDC(), pos.x - cam.left, pos.y - cam.top, 70, 70);
+	//Rectangle(getMemDC(), rc.left - cam.left, rc.top - cam.top, rc.right - cam.left, rc.bottom - cam.top);
+	//RectangleMake(getMemDC(), pos.x - cam.left, _probeY - cam.top, 10, 10);
+	//RectangleMake(getMemDC(),_probeX- cam.left, pos.y - cam.top, 10, 10);
 
+	will->frameRender(getMemDC(), pos.x - cam.left, pos.y - cam.top);
 
-
+	if (KEYMANAGER->isToggleKey(VK_DELETE))
+	{
+		Rectangle(getMemDC(), _rc);
+	}
+	if (KEYMANAGER->isToggleKey(VK_END))
+	{
+		Rectangle(getMemDC(), _rcProbe);
+	}
 	
 
 }
 
+
+//에너미와 충돌처리
+void player::collision()
+{
+	RECT temp;
+	//findObjects는 해당 타입과 키값을 가지고 있는 클래스를 벡터에 담아서 값을 반환 해주는 역할을 합니다.
+	//포인터! 같은 주소값을 가르키고 있습니다. 
+	vector<gameObject*> _mini = OBJECTMANAGER->findObjects(objectType::ENEMY, "enemy");
+
+	
+	for (int i = 0; i < _mini.size(); ++i)
+	{
+		if (IntersectRect(&temp, &rc, &_mini[i]->rc))
+		{
+			//오브젝트 매니저 업데이트로 가보면
+			/*
+			//만약 오브젝트가 살아있지않다면
+			if (miter->second[i]->isLive() == false)
+			{
+				miter->second[i]->release();
+				SAFE_DELETE(miter->second[i]);
+				miter->second.erase(miter->second.begin() + i);
+				--i;
+				continue;
+			}
+			
+			라는 코드가 있는데 충돌이 됬으면 mini는 죽었어! 라고 오브젝트 매니저쪽에 Set함수로 신호를 보냅니다 
+			그럼 오브젝트 매니저 쪽에서 벡터를 삭제 합니다. 
+			고로 우린 일일히 벡터를 지우고 어쩌고 안해도 죽었다고 isLive = false로 바꿔주는것만으로도 벡터를 삭제해줍니다.
+
+			참고로 우린 에너미에게 직접접근이 아닌 오브젝트 매니저로 간접접근을 하고 있기에 객체지향에 맞는 코딩을 하고 있는것입니다.
+
+			*/
+			_mini[i]->setIsLive(false);
+			
+		}
+	}
+
+
+}
+
+void player::move()
+{
+	++_count;
+
+	//if(_scene == 상점 or 마을)
+	if (_isRolling != true)
+	{
+		if (KEYMANAGER->isStayKeyDown(VK_UP))
+		{
+			_isUp = true;
+			will->setFrameX(_index);
+			will->setFrameY(4);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+				}
+			}
+			pos.y -= _speed;
+			_probeX = pos.x;
+			_probeY = pos.y;
+			for (int i = _probeY +10; i < _probeY + 30; ++i)
+			{
+				COLORREF color = GetPixel(IMAGEMANAGER->findImage("redZone")->getMemDC(), _probeX, i);
+				int r = GetRValue(color);
+				int g = GetGValue(color);
+				int b = GetBValue(color);
+
+				if (r == 255 && g == 0 && b == 0)
+				{
+					pos.y = i;
+					break;
+				}
+			}
+		}
+		if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+		{
+			_isUp = false;
+			will->setFrameX(_index);
+			will->setFrameY(5);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+				}
+			}
+			pos.y += _speed;
+			_probeX = pos.x;
+			_probeY = pos.y + will->getFrameHeight();
+			for (int i = _probeY - 10; i < _probeY - 30; --i)
+			{
+				COLORREF color = GetPixel(IMAGEMANAGER->findImage("redZone")->getMemDC(), _probeX, i);
+				int r = GetRValue(color);
+				int g = GetGValue(color);
+				int b = GetBValue(color);
+
+				if (r == 255 && g == 0 && b == 0)
+				{
+					pos.y = _probeY;
+					break;
+				}
+			}
+		}
+		if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+		{
+			_isRight = true;
+			will->setFrameX(_index);
+			will->setFrameY(6);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+				}
+			}
+			pos.x += _speed;
+			_probeX = pos.x + will->getFrameWidth();
+			_probeY = pos.y;
+			for (int i = _probeX - 10; i < _probeX - 30; --i)
+			{
+				COLORREF color = GetPixel(IMAGEMANAGER->findImage("redZone")->getMemDC(), i, _probeY);
+				int r = GetRValue(color);
+				int g = GetGValue(color);
+				int b = GetBValue(color);
+
+				if (r == 255 && g == 0 && b == 0)
+				{
+					pos.x = i;
+					break;
+				}
+			}
+		}
+		if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+		{
+			_isRight = false;
+			will->setFrameX(_index);
+			will->setFrameY(7);
+			if (_count % 10 == 0)
+			{
+				--_index;
+				if (_index < 0)
+				{
+					_index = 7;
+				}
+			}
+			pos.x -= _speed;
+			_probeX = pos.x - will->getFrameWidth()/2;
+			_probeY = pos.y;
+			for (int i = _probeX - 30; i < _probeX + 30; ++i)
+			{
+				COLORREF color = GetPixel(IMAGEMANAGER->findImage("redZone")->getMemDC(), i, _probeY);
+				int r = GetRValue(color);
+				int g = GetGValue(color);
+				int b = GetBValue(color);
+
+				if (r == 255 && g == 0 && b == 0)
+				{
+					pos.x = i;
+					break;
+				}
+			}
+
+		}
+		if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
+		{
+			_index = 0;
+			_isRolling = true;
+			_isInvincible = true;
+			_acceleration = 0.5f;
+		}
+	}
+	else
+	{
+		if (_isUp)
+		{
+			will->setFrameX(_index);
+			will->setFrameY(8);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+					_isRolling = false;
+					_isInvincible = false;
+				}
+			}
+			
+			pos.y -= (_speed + _acceleration);
+		}
+		if (_isUp == false)
+		{
+			will->setFrameX(_index);
+			will->setFrameY(9);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+					_isRolling = false;
+					_isInvincible = false;
+				}
+			}
+			pos.y += (_speed + _acceleration);
+		}
+		if (_isRight)
+		{
+			will->setFrameX(_index);
+			will->setFrameY(10);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+					_isRolling = false;
+					_isInvincible = false;
+				}
+			}
+			pos.x += (_speed + _acceleration);
+		}
+		if(_isRight == false)
+		{
+			will->setFrameX(_index);
+			will->setFrameY(11);
+			if (_count % 10 == 0)
+			{
+				++_index;
+				if (_index > 7)
+				{
+					_index = 0;
+					_isRolling = false;
+					_isInvincible = false;
+				}
+			}
+			pos.x -= (_speed + _acceleration);
+		}
+	}
+	_rc = RectMake(pos.x, pos.y, will->getFrameWidth(), will->getFrameHeight());
+	_rcProbe = RectMake(_probeX, _probeY, _probeX + 30, _probeY + 30);
+	
+		
+	
+	
+	
+
+
+	/*
+	//else if((_scene == 던전 )
+	if (KEYMANAGER->isStayKeyDown(VK_UP))
+	{
+		pos.y -= speed;
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+	{
+		pos.y += speed;
+
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+	{
+		pos.x -= speed;
+	}
+	if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+	{
+		pos.x += speed;
+	}
+	if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
+	{
+
+	}
+
+	*/
+}
--- moonLighter/player.h
+++ moonLighter/player.h
@@ -2,16 +2,39 @@
 #include "gameNode.h"
 #include "gameObject.h"
 
+class mini;
 
 class player : public gameObject
 {
 private:
 	
+	int _index;
+	int _count;
+	int _probeY;
+	int _probeX;
+
+	float _time;
+	float _speed;
+	float _acceleration;
+	bool _isUp;
+	bool _isRight;
+	bool _isRolling;
+	bool _isInvincible;
+
+	image* backGround;
+	image* will;
+
+	RECT _rc;
+	RECT _rcProbe;
 public:
 	HRESULT init(string _objName, tagFloat _pos);
 	void release();
 	void update();
 	void render();
+
+	void collision();
+	void move();
+	bool getInvincible() { return _isInvincible; }
 
 
 	player() {}
