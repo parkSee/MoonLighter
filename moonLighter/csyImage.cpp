8a580d776ecb6dde6b4c57b642d57e8de4f197b4
채수윤 <tndbsdlchlrh@naver.com>
2018-07-28 21:10:17 +0900

    인벤토리 테스트용 이미지 추가

--- moonLighter/csyImage.cpp
+++ moonLighter/csyImage.cpp
@@ -12,5 +12,5 @@ void loadingScene::csyImageLoading()
 	_loading->loadImage("rebar", "./Image/item_object/item_rebar.bmp", 20, 34, true, MAGENTA);
 	_loading->loadImage("redCristal", "./Image/item_object/item_redCristal.bmp", 28, 36, true, MAGENTA);
 	_loading->loadImage("golemCore", "./Image/item_object/item_golemCore.bmp", 24, 24, true, MAGENTA);
-
+	_loading->loadImage("inventory", "./Image/item_object/inven.bmp", 828, 423, true, MAGENTA);
 }
