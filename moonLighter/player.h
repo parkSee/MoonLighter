b3d1b699ebda2ea50d09138e73889ae639ded759
이요성 <leeys1551@gmail.com>
2018-07-28 19:20:35 +0900

    bool getIsRcSwordOn() { return _isRcSwordOn; } //lysADD (공격중인지 아닌지 bool값 반환)

--- moonLighter/player.h
+++ moonLighter/player.h
@@ -53,7 +53,7 @@ public:
 	float getSpeed() { return _speed; }		//lysADD  (겟스피드 함수 추가)
 	RECT getRcBody() { return _rcBody; }	//lysADD  (겟몸뚱아리 렉트 함수 추가)
 	RECT getRcSword() { return _rcSword;}	//lysADD (겟 공격범위 렉트 함수 추가)
-
+	bool getIsRcSwordOn() { return _isRcSwordOn; } //lysADD (공격중인지 아닌지 bool값 반환)
 	player() {}
 	~player() {}
 };
