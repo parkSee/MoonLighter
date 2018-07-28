34e9b98fd2a74759492a969bf9766551a0996c9f
이요성 <leeys1551@gmail.com>
2018-07-28 13:40:05 +0900

    progressBar 내가 쓸거 수정했고, 플레이어 이미지 이름 수정했고, 체력바 이미지 추가했고, s키 누르면 체력바 테스트 할 수 있고, 게임 오브젝트 변수명 언더바 써줬슴

--- moonLighter/lysImage.cpp
+++ moonLighter/lysImage.cpp
@@ -5,9 +5,9 @@
 
 void loadingScene::lysImageLoading()
 {
-	_loading->loadFrameImage("will", "Image/will_shop2.bmp", 1800, 2160, 10, 12, true, RGB(255, 0, 255));
-
-	//lysADD(공격이미지 추가)
-	_loading->loadFrameImage("will_shortAttack", "Image/will_shortAttack2.bmp", 1344, 816, 8, 4, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("will", "Image/player/will_shop.bmp", 1800, 2160, 10, 12, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("will_shortAttack", "Image/player/will_shortAttack.bmp", 1344, 816, 8, 4, true, RGB(255, 0, 255));
+	_loading->loadImage("will_hpBar", "Image/player/hpBar.bmp", 150, 50, 130, 228, true, RGB(255, 0, 255));  //lysADD(체력바 이미지 로딩)
+	
 }
 
