fbeea71aede6cb35b3127489c405c3a8f488a030
박시영 <soso667@naver.com>
2018-07-30 11:09:57 +0900

    AI이동 다시 올립니다

--- moonLighter/shopScene.h
+++ moonLighter/shopScene.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "gameNode.h"
 #include "player.h"
-<<<<<<< HEAD
+
 #include "AIKids.h"
-=======
+
 #include "inventory.h"
->>>>>>> 인벤토리 테스트용 코드, 이미지 추가
+
 
 class shopScene : public gameNode
 {
