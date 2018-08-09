13317b7ff7b9e484c0b2dc469bea0c77e23a61da
이요성 <leeys1551@gmail.com>
2018-08-10 00:49:42 +0900

    울게 하지 마요

--- moonLighter/player.h
+++ moonLighter/player.h
@@ -18,7 +18,7 @@ typedef struct tagArrow
 	tagFloat pos;
 	int speed;
 	bool isActive;
-	RECT rcArrow;
+	RECT rc;
 	direction dir;
 } Arrow;
 
@@ -132,6 +132,7 @@ public:
 	void bowFrameUpdate();
 	void arrowUpdate();
 	void attackRender();
+	void arrowRender();
 	void noUsePendant();
 	void goHome();
 	void numberUpdate();
